6d5d61f843cd7db799d668322721fe7a64f9e475
Tony Wang <tonywang@TonydeMacBook-Pro-2.local>
2017-04-23 14:13:47 +0800

    add website

--- app.js
+++ app.js
@@ -91,7 +91,11 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
-
+// Serve a static web page
+server.get(/.*/, restify.serveStatic({
+  'directory': '.',
+  'default': 'index.html'
+}));
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
