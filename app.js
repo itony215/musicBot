68dfb700740be3c94d3ec94b8612b483597ecbc8
Tony Wang <tonywang@TonydeMacBook-Pro-2.local>
2017-04-23 11:58:38 +0800

    v2 delete mark

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-//如何寫成一個table
 const kill = {
   '自殺1': {
     title: '自殺1',
@@ -64,9 +63,9 @@ const fuck = {
 const grace = {
   '恩典1': {
     title: '恩典之路',
-    subtitle: '',
-    image_url: "https://en.wikipedia.org/wiki/Space_Needle.jpg",
-    item_url: 'http://wwe'
+    subtitle: '我的心情現在適合這首歌',
+    image_url: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTKqtJAouJ7Ys3qt2gfM_DH21_O18lWnKgbt47Ve0NtUhGUK2zh",
+    item_url: 'https://www.youtube.com/watch?v=0Q444bzsehU'
   },
   '恩典2': {
     title: '恩典之路2',
@@ -88,17 +87,11 @@ var builder = require('botbuilder');
 var restify = require('restify');
 var request = require('request');
 var cheerio = require('cheerio');
-//=========================================================
-// Bot Setup
-//=========================================================
-
-// Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat bot
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
@@ -106,8 +99,6 @@ var connector = new builder.ChatConnector({
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-
-// Create LUIS recognizer that points at our model and add it as the root '/' dialog for our Cortana Bot.
 var model = 'https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/01f14fbd-d409-4951-b638-080f5867ec65?subscription-key=93971faefdc84030b897dbdca188cde2&verbose=true&timezoneOffset=0.0&q=';
 const YOUTUBE_API = `https://www.youtube.com/results?search_query=`;
 
@@ -122,49 +113,39 @@ bot.dialog('/moodMusic', intents);
 
 intents.matches('點歌', [
   function (session, args, next) {
-    console.log(args.entities);
     var task = builder.EntityRecognizer.findEntity(args.entities, '歌曲');
     var singer = builder.EntityRecognizer.findEntity(args.entities, '歌手');
     if (!task) {
       if (singer.entity) {
         builder.Prompts.text(session, '想聽' + singer.entity.toString() + '的什麼歌呢?');
-        //缺傳入歌手
       } else {
         builder.Prompts.text(session, "什麼歌名呢?");
       }
     } else {
       if (!singer) {
-        next({ response: task.entity });//代替user的回應
+        next({ response: task.entity });
       } else {
-        next({ response: task.entity, response2: singer.entity });//代替user的回應
+        next({ response: task.entity, response2: singer.entity });
       }
     }
   },
   function (session, results) {
-    console.log('！！！！session: %j,results : %j', session, results);
     if (results.response) {
-      // ... save task
       const youtubeUrl = `${YOUTUBE_API}${encodeURIComponent(results.response)}`;
-      console.log(youtubeUrl);
       request({ uri: youtubeUrl }, function (error, response, body) {
-
-        console.log('body:', body);
         const $ = cheerio.load(body);
         const href = $('.yt-lockup-title > a').attr('href');
         const img_href = $('.yt-thumb-simple > img').attr('src');
         const videoHref = `https://www.youtube.com${href}`;
-        console.log('href', videoHref);
         msg = new builder.Message(session);
         msg.attachments([{
           contentType: "image/jpeg",
           contentUrl: img_href
         }]);
-
         session.send(msg);
         session.send(`[${videoHref}](${videoHref})`);
-        //console.log('error2:', error2); // Print the error if one occurred
       });
-      session.send('好喔~ "%s"的連結跟縮圖來囉！', results.response);
+      session.endDialog('好喔~ "%s"的連結跟預覽圖來囉！', results.response);
     } else {
       session.send("oops!");//error handling
     }
@@ -175,7 +156,6 @@ let matchProp = '';
 intents.matches('心情', [
   function (session) {
     if (session.message.text) {
-      // ... save task
       let isMatch = false;
       Object.keys(DATA).forEach((prop) => {
         if (isMatch) {
@@ -188,8 +168,7 @@ intents.matches('心情', [
           matchProp = '恩典';
         }
       })
-      console.log('matchProp: ', DATA[matchProp]);
-      builder.Prompts.choice(session, "Ok... play the", DATA[matchProp]);
+      builder.Prompts.choice(session, "我能理解你的心情，推薦三首好歌給您選擇唷！", DATA[matchProp]);
     } else {
       session.send("Ok");
     }
@@ -198,7 +177,6 @@ intents.matches('心情', [
 
     if (results.response) {
       var mood = DATA[matchProp][results.response.entity];
-      console.log('moooooooood        ', mood.title);
       msg = new builder.Message(session);
       msg.sourceEvent({
         facebook: {
@@ -207,10 +185,10 @@ intents.matches('心情', [
             payload: {
               template_type: "generic",
               elements: [{
-                title: 'ddd',
-                subtitle: "context",
-                image_url: "https://en.wikipedia.org/wiki/Space_Needle.jpg",
-                item_url: "http://m.me",
+                title: mood.title,
+                subtitle: mood.subtitle,
+                image_url: mood.image_url,
+                item_url: mood.item_url,
                 buttons: [{
                   type: "element_share"
                 }]
@@ -224,10 +202,10 @@ intents.matches('心情', [
       } else {
         msg.attachments([{
           contentType: "image/jpeg",
-          contentUrl: "http://www.theoldrobots.com/images62/Bender-18.JPG"
+          contentUrl: mood.image_url
         }]);
         session.send(msg);
-        session.endDialog("%(image_url)s", mood);
+        session.endDialog("%(item_url)s", mood);
       }
 
     } else {
